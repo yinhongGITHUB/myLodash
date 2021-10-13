1dd749b6e7be6d95c99ed5abbfad2998975187ab
PleaseLetMe(yinhong) <yinhong@xunlei.com>
2021-10-13 20:37:27 +0800

    优化

--- Array/compact.js
+++ Array/compact.js
@@ -1,19 +1,17 @@
 /**
  * 
  * @param {任意数组} arr 
- * @returns 
+ * @returns {返回的是一个每一项都为true的新数组}
  */
-let myCompact = function(arr) {
+let myCompact = function (arr) {
     // 先做参数校验
     if (!(arr instanceof Array)) {
         throw "请传入正确的参数";
     }
     let newArr = []
     arr.forEach(item => {
-        if(item){
-            newArr.push(item)
-        }
+        if (item) newArr.push(item)
     });
     return newArr
 }
-module.exports =  myCompact
+module.exports = myCompact
